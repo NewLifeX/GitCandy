bca3485792a4749b1870f5581fe5ea80e2c5b4b5
大石头 <nnhy@vip.qq.com>
2016-11-25 00:02:45 +0800

    配置文件加上注释

--- GitCandy/Configuration/UserConfiguration.cs
+++ GitCandy/Configuration/UserConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using NewLife.Xml;
@@ -10,33 +11,61 @@ namespace GitCandy.Configuration
     public class UserConfiguration : XmlConfig<UserConfiguration>
     {
         #region 属性
+        /// <summary>开放服务</summary>
+        [DisplayName("开放服务")]
         public bool IsPublicServer { get; set; } = true;
 
+        /// <summary>强制SSL</summary>
+        [DisplayName("强制SSL")]
         public bool ForceSsl { get; set; }
 
+        /// <summary>SSL端口</summary>
+        [DisplayName("SSL端口")]
         public int SslPort { get; set; } = 443;
 
-        public bool LocalSkipCustomError { get; set; }
+        /// <summary>开启SSH</summary>
+        [DisplayName("开启SSH")]
+        public bool EnableSsh { get; set; } = true;
 
-        public bool AllowRegisterUser { get; set; } = true;
-
-        public bool AllowRepositoryCreation { get; set; } = true;
-
-        public String RepositoryPath { get; set; } = "..\\Repos";
-
-        public String CachePath { get; set; } = "..\\Cache";
-
-        public String GitCorePath { get; set; }
-
-        public int Commits { get; set; } = 30;
-
-        public int PageSize { get; set; } = 30;
-
-        public int Contributors { get; set; } = 50;
-
+        /// <summary>SSH端口</summary>
+        [DisplayName("SSH端口")]
         public int SshPort { get; set; } = 22;
 
-        public bool EnableSsh { get; set; } = true;
+        /// <summary>跳过本地错误</summary>
+        [DisplayName("跳过本地错误")]
+        public bool LocalSkipCustomError { get; set; }
+
+        /// <summary>允许注册</summary>
+        [DisplayName("允许注册")]
+        public bool AllowRegisterUser { get; set; } = true;
+
+        /// <summary>允许创建代码库</summary>
+        [DisplayName("允许创建代码库")]
+        public bool AllowRepositoryCreation { get; set; } = true;
+
+        /// <summary>代码库存储路径</summary>
+        [DisplayName("代码库存储路径")]
+        public String RepositoryPath { get; set; } = "..\\Repos";
+
+        /// <summary>缓存路径</summary>
+        [DisplayName("缓存路径")]
+        public String CachePath { get; set; } = "..\\Cache";
+
+        /// <summary>Git-Core路径</summary>
+        [DisplayName("Git-Core路径")]
+        public String GitCorePath { get; set; }
+
+        /// <summary>每页提交数</summary>
+        [DisplayName("每页提交数")]
+        public int Commits { get; set; } = 30;
+
+        /// <summary>分页大小</summary>
+        [DisplayName("分页大小")]
+        public int PageSize { get; set; } = 30;
+
+        /// <summary>显示参与者数</summary>
+        [DisplayName("显示参与者数")]
+        public int Contributors { get; set; } = 50;
 
         public List<HostKey> HostKeys { get; set; } = new List<HostKey>();
         #endregion
