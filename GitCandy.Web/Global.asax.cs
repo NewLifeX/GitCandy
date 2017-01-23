bf6d1079f0caa399a3985df9395ee805dfead57d
大石头X2 <nnhy@vip.qq.com>
2017-01-23 13:44:27 +0800

    修正无法进入后台管理的BUG

--- GitCandy.Web/Global.asax.cs
+++ GitCandy.Web/Global.asax.cs
@@ -12,94 +12,18 @@ namespace GitCandy
 {
     public class GitCandyApplication : System.Web.HttpApplication
     {
-        public bool HidingRequestResponse { get; private set; }
-
         protected void Application_Start()
         {
-            HidingRequestResponse = HttpRuntime.UsingIntegratedPipeline;
-
-            //Logger.SetLogPath();
-            //XTrace.WriteLine(AppInfomation.GetAppStartingInfo());
             XTrace.WriteVersion(this.GetType().Assembly);
-
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            //BundleConfig.RegisterBundles(BundleTable.Bundles);
 
             AreaRegistration.RegisterAllAreas();
 
-            //MefConfig.RegisterMef();
-            //ScheduleConfig.RegisterScheduler();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
 
             GitCacheAccessor.Initialize();
             SshServerConfig.StartSshServer();
-
-            //XTrace.WriteLine(AppInfomation.GetAppStartedInfo());
-
-            HidingRequestResponse = false;
         }
-
-        //protected void Application_End()
-        //{
-        //    SshServerConfig.StopSshServer();
-        //    ScheduleConfig.StopAndWait();
-        //    //XTrace.WriteLine(AppInfomation.GetAppEndInfo());
-
-        //    Process.GetCurrentProcess().Kill();
-        //}
-
-        //protected void Application_Error()
-        //{
-        //    var context = this.Context;
-        //    var server = context.Server;
-        //    var ex = server.GetLastError();
-        //    var statusCode = new HttpException(null, ex).GetHttpCode();
-
-        //    var sb = new StringBuilder();
-        //    if (HidingRequestResponse)
-        //        sb.AppendLine(statusCode + ", Unknow, First request on integrated mode");
-        //    else
-        //        sb.AppendLine(statusCode + ", " + context.Request.HttpMethod + ", " + context.Request.Url.ToString());
-        //    if (statusCode == 500)
-        //    {
-        //        sb.AppendLine(ex.ToString());
-        //        XTrace.Log.Error(sb.ToString());
-        //    }
-        //    else
-        //    {
-        //        XTrace.Log.Warn(sb.ToString());
-        //    }
-
-        //    if (!HidingRequestResponse && UserConfiguration.Current.LocalSkipCustomError && context.Request.IsLocal)
-        //        return;
-
-        //    if (!HidingRequestResponse)
-        //    {
-        //        var response = context.Response;
-        //        response.Clear();
-        //        response.StatusCode = statusCode;
-        //        response.TrySkipIisCustomErrors = true;
-        //        response.ContentType = @"text/html; charset=utf-8";
-
-        //        var path = server.MapPath("~/CustomErrors/");
-        //        var filename = Path.Combine(path, statusCode + ".html");
-        //        if (File.Exists(filename))
-        //        {
-        //            response.WriteFile(filename);
-        //        }
-        //        else
-        //        {
-        //            filename = Path.Combine(path, "000.html");
-        //            if (File.Exists(filename))
-        //            {
-        //                var content = File.ReadAllText(filename);
-        //                response.Write(String.Format(content, statusCode));
-        //            }
-        //        }
-        //    }
-
-        //    server.ClearError();
-        //}
 
         protected void Application_AcquireRequestState()
         {
