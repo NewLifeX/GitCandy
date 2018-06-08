cfb66746a329d9b606ae32e059cf72bff6cd45f9
mstarhand <68@qq.com>
2018-06-08 14:51:16 +0800

    恢复误删的样式文件，修复因默认密码位数不够6位导致的密码无法修改的问题

--- GitCandy.Web/Models/ChangePasswordModel.cs
+++ GitCandy.Web/Models/ChangePasswordModel.cs
@@ -7,7 +7,7 @@ namespace GitCandy.Models
     public class ChangePasswordModel
     {
         [Required(ErrorMessageResourceType = typeof(SR), ErrorMessageResourceName = "Validation_Required")]
-        [StringLength(100, MinimumLength = 6, ErrorMessageResourceType = typeof(SR), ErrorMessageResourceName = "Validation_StringLengthRange")]
+        [StringLength(100, MinimumLength =5, ErrorMessageResourceType = typeof(SR), ErrorMessageResourceName = "Validation_StringLengthRange")]
         [DataType(DataType.Password)]
         [Display(ResourceType = typeof(SR), Name = "Account_OldPassword")]
         public String OldPassword { get; set; }
